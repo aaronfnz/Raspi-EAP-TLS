9fdccd9b17bd8cc651d19fc9ccc90bb405996ec1
Aaron Farnell <121586817+aaronfnz@users.noreply.github.com>
2024-04-06 10:21:19 +1300

    Add files via upload

---
+++ create-eap-tls-config.ps1
@@ -0,0 +1,11 @@
+#Read in parameters
+$ssidName = Read-Host "SSID Name"
+$p12CertPath = Read-Host "Path to P12 file"
+
+#Open SSL extract cert components
+
+
+$caCertPath = "ca-root.pem"
+$privateKeyPath = "user.key.pem"
+$clientCertPath = "client.crt.pem"
+

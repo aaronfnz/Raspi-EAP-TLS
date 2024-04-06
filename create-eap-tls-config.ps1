05dfcdc1039dd8d6a1e8659ecacc96972e860d46
aaronfnz <aaron.farnell@hotmail.com>
2024-04-06 15:06:27 +1300

    config files

--- create-eap-tls-config.ps1
+++ create-eap-tls-config.ps1
@@ -15,3 +15,37 @@ $clientCertPath = "/etc/wpa_supplicant/certs/client.crt.pem"
 #Root CA
 #openssl pkcs12 -in $p12CertPath -cacerts -nokeys -chain -out $caCertPath -password pass:$p12Password
 
+#Create WPA config file
+$wpaConfig = ("
+ctrl_interface=/var/run/wpa_supplicant
+update_config=1
+country=NZ
+
+network={
+    ssid=`"$ssidName`"
+    key_mgmt=WPA-EAP
+    eap=TLS
+    identity=`"info@domain.com`"
+    ca_cert=`"$caCertPath`"
+    client_cert=`"$clientCertPath`"
+    private_key=`"$privateKeyPath`"
+    private_key_passwd=`"$p12Password`"
+}")
+$wpaConfig | Out-File c:\temp\wpa_supplicant.conf
+
+#Create Interface Config file
+$intConfig = ("
+source-directory /etc/network/interfaces.d
+
+auto lo
+
+iface lo inet loopback
+iface eth0 inet dhcp
+
+allow-hotplug wlan0
+
+iface wlan0 inet dhcp
+    pre-up wpa_supplicant -B -Dwext -i wlan0 -c/etc/wpa_supplicant/wpa_supplicant.conf
+    post-down killall -q wpa_supplicant   
+")
+$intConfig | Out-File c:\temp\Interfaces

59a7cc37d58fae3ae2dc002d65a8cc6dffb345fc
aaronfnz <aaron.farnell@hotmail.com>
2024-04-06 22:22:32 +1300

    updates

--- create-eap-tls-config.sh
+++ create-eap-tls-config.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
+clear
+
 read -p "SSID Name: " ssidName
-read -p "P12 cert to import: " p12CertPath
+read -e -p "P12 cert to import: " p12CertPath
 read -sp "Cert password: " p12Password
 
-echo $ssidName
-echo $p12CertPath
-echo $p12Password
+if test ! -d "/etc/wpa_supplicant/certs"; then
+    echo "certs dir not found, creating it now"
+    #mkdir /Users/aaron/certs
+fi
+
+#Cert names and storage paths
+caCertPath="/Users/aaron/certs/ca-root.pem"
+privateKeyPath="/Users/aaron/certs/user.key.pem"
+clientCertPath="/Users/aaron/certs/client.crt.pem"
+
+echo "Password is: " $p12Password
+
+#Open SSL extract cert components
+#Private Key
+eval "openssl pkcs12 -in "$p12CertPath "-nocerts -nodes -out "$privateKeyPath "-password pass:"$p12Password
+#Certificate
+#openssl pkcs12 -in $p12CertPath -clcerts -nokeys -out $clientCertPath -password pass:$p12Password
+#Root CA
+#openssl pkcs12 -in $p12CertPath -cacerts -nokeys -chain -out $caCertPath -password pass:$p12Password

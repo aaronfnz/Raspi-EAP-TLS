25e7b26b51cb96a437e2a39025a86f92d3333144
aaronfnz <aaron.farnell@hotmail.com>
2024-04-07 20:18:10 +1200

    fixes

--- create-eap-tls-config.sh
+++ create-eap-tls-config.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
+
+#chmod +x create-eap-tls-config.sh
+#execute with: ./create-eap-tls-config.sh
+
 clear
 read -p "SSID Name: " ssidName
-read -e -p "P12 cert to import: " p12CertPath
+read -ep "P12 cert to import: " p12CertPath
 read -sp "Cert password: " p12Password
 
 #this don't work right.
 if test ! -d "/etc/wpa_supplicant/certs"; then
     echo "certs dir not found, creating it now"
-    mkdir /etc/wpa_supplicant/certs
+    mkdir -p /etc/wpa_supplicant/certs
 fi
 
 #Cert names and storage paths
 caCertPath="/etc/wpa_supplicant/certs/ca-root.pem"
 privateKeyPath="/etc/wpa_supplicant/certs/user.key.pem"
-clientCertPath="/etc/wpa_supplicant/certs/client.crt.pem"
+clientCertPath="/etc/wpa_supplicant/certs/user.crt.pem"
 
 #Open SSL extract cert components
 #Private Key
@@ -24,7 +28,7 @@ eval "openssl pkcs12 -in "$p12CertPath "-clcerts -nokeys -out "$clientCertPath "
 eval "openssl pkcs12 -in "$p12CertPath "-cacerts -nokeys -chain -out "$caCertPath "-password pass:"$p12Password
 
 #Create WPA config file
-cat > /etc/wpa_supplicant/certs/wpa_supplicant.config <<- EOM
+cat > /etc/wpa_supplicant/wpa_supplicant.config <<- EOM
 ctrl_interface=/var/run/wpa_supplicant
 update_config=1
 country=NZ

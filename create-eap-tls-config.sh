6fa421fb3737502523936e1dc16fa43a4e5aae92
aaronfnz <aaron.farnell@hotmail.com>
2024-04-07 19:32:38 +1200

    config

--- create-eap-tls-config.sh
+++ create-eap-tls-config.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 clear
-
 read -p "SSID Name: " ssidName
 read -e -p "P12 cert to import: " p12CertPath
 read -sp "Cert password: " p12Password
@@ -25,20 +24,19 @@ eval "openssl pkcs12 -in "$p12CertPath "-clcerts -nokeys -out "$clientCertPath "
 eval "openssl pkcs12 -in "$p12CertPath "-cacerts -nokeys -chain -out "$caCertPath "-password pass:"$p12Password
 
 #Create WPA config file
-read -r -d '' wpa_supplicant << EOM
+cat > /c/temp/wpa_supplicant.config <<- EOM
 ctrl_interface=/var/run/wpa_supplicant
 update_config=1
 country=NZ
 
 network={
-    ssid=$ssidName
+    ssid="$ssidName"
     key_mgmt=WPA-EAP
-    eap=TLS\n
+    eap=TLS
     identity=info@domain.com
-    ca_cert=$caCertPath
-    client_cert=$clientCertPath
-    private_key=$privateKeyPath
-    private_key_passwd=$p12Password
+    ca_cert="$caCertPath"
+    client_cert="$clientCertPath"
+    private_key="$privateKeyPath"
+    private_key_passwd="$p12Password"
 }
 EOM
-echo "$wpa_supplicant"

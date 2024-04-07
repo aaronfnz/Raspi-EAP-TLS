c103a2eb01ed147a1a60a8e915d61c35a6968322
aaronfnz <aaron.farnell@hotmail.com>
2024-04-07 15:19:18 +1200

    outputs

--- create-eap-tls-config.sh
+++ create-eap-tls-config.sh
@@ -23,3 +23,22 @@ eval "openssl pkcs12 -in "$p12CertPath "-nocerts -nodes -out "$privateKeyPath "-
 eval "openssl pkcs12 -in "$p12CertPath "-clcerts -nokeys -out "$clientCertPath "-password pass:"$p12Password
 #Root CA
 eval "openssl pkcs12 -in "$p12CertPath "-cacerts -nokeys -chain -out "$caCertPath "-password pass:"$p12Password
+
+#Create WPA config file
+read -r -d '' wpa_supplicant << EOM
+ctrl_interface=/var/run/wpa_supplicant
+update_config=1
+country=NZ
+
+network={
+    ssid=$ssidName
+    key_mgmt=WPA-EAP
+    eap=TLS\n
+    identity=info@domain.com
+    ca_cert=$caCertPath
+    client_cert=$clientCertPath
+    private_key=$privateKeyPath
+    private_key_passwd=$p12Password
+}
+EOM
+echo "$wpa_supplicant"
